46e8dbaf1fe63ff88361052c938eb7e6b9b94c6c
Nathaly Sotomayor <nathysoto12.07@gmail.com>
2022-02-19 20:13:56 -0500

    Change logic

--- 0x07-linked_list_cycle/0-check_cycle.c
+++ 0x07-linked_list_cycle/0-check_cycle.c
@@ -1,26 +1,19 @@
 #include "lists.h"
 
 /**
- * check_cycle - detect cycles
- * @list: head
- * Return: 0 | 1
+ * check_cycle - checks if a singly linked list has a cycle in it
+ * @list: pointer to head of list
+ * Return: 0 if there is no cycle, 1 if there is a cycle
  */
 int check_cycle(listint_t *list)
 {
-	listint_t *t = NULL;
-	listint_t *r = NULL;
+	listint_t *slow = list, *fast = list;
 
-	if (!list)
-		return (0);
-
-	t = list;
-	r = list;
-
-	while (t && r)
+	while (fast && fast->next)
 	{
-		t = t->next;
-		r = r->next->next;
-		if (t == r)
+		slow = slow->next;
+		fast = fast->next->next;
+		if (slow == fast)
 			return (1);
 	}
 	return (0);
